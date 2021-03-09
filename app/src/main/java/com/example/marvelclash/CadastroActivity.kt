c22b7c1ffe1bf4ba31d8b0754567125f8437bc55
thaiscocuzza <thais_cocuzza@hotmail.com>
2021-03-08 22:52:22 -0300

    telas login + splash. Falta os codigos

---
+++ app/src/main/java/com/example/marvelclash/CadastroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marvelclash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CadastroActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela_cadastro)
+    }
+}
